23b9d0c148a9db169ec2d67dc4ecd154cecb9730
maxtenton <maxtenton@gmail.com>
2025-10-17 09:31:13 +0300

    initial commit

---
+++ cveval.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import os
+
+load_dotenv()
+Key = os.environ["API_KEY"]
+
+client = genai.Client(api_key=Key)
+prompt = open("prompt.md", "r", encoding='utf-8').read()
+
+def CheckCV(CV = "", num = 0):
+    global client
+    global JD
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"{JD}{prompt}{CV}",
+        config=types.GenerateContentConfig(
+            temperature=0.2
+        )
+    )
+    with open(f"output/cv{1}.json", "w") as file:
+        file.write(response)
+    MDresponse = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"Make this JSON file into md file : {response}",
+        config=types.GenerateContentConfig(
+            temperature=0.2
+        )
+    )
+    with open(f"output/cv{1}.md", "w") as file:
+        file.write(MDresponse)
+
+
+with open("sample_inputs/jd.txt", "r") as file:
+    global JD
+    JD = file.read()
+
+for i in range(1, 3):
+    with open(f"sample_input/cv{i}.txt", "r") as file:
+        CheckCV(CV = file.read(), num = i)
+
---
+++ prompt.md
@@ -0,0 +1,14 @@
+using this job description(JD) evaluate the following CV and return the evaluation result in following json format: 
+{
+ "match_score": 0-100,
+ "summary": "Īss apraksts, cik labi CV atbilst JD.",
+ "strengths": [
+ "Galvenās prasmes/pieredze no CV, kas atbilst JD"
+ ],
+ "missing_requirements": [
+ "Svarīgas JD prasības, kas CV nav redzamas"
+ ],
+ "verdict": "strong match | possible match | not a match"
+}
+
+The CV : 
